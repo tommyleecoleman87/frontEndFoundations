0dca1c425ceaf47c18dcf223366cd1c9de7e6f68
Tommy <tommyleecoleman87@gmail.com>
2018-04-08 19:36:38 -0400

    working on final project

--- Front End Foundations/Final Project/script.js
+++ Front End Foundations/Final Project/script.js
@@ -7,3 +7,36 @@ $(document).ready(function(){
         $(this).css('width', '50px');
     });
 });
+
+function loadRepo(url, callback) {
+    const gitHubRequest = new XMLHttpRequest();
+      // Finish your request here
+      gitHubRequest.onreadystatechange = function() {
+          if (this.readyState === 4 && this.status === 200) {
+              callback(this);
+          }
+      }
+      // Don't forget the callback()
+  }
+  
+  function loadRepoCallback(gitHubRequest) {
+    // Callback function needs to parse the JSON
+    // Using the DOM get the element ID from your <ul> in your HTML
+    // You will need a forEach to loop through the parsed object
+    // Using the DOM create new list elements
+    // Plug in the .html_url and .name from the parsed object.  To make this a link you'll need 
+    // Append the new list items to the element you retrieved from HTML
+  }
+
+$(document).ready(function(){
+    $('.greyTheme').click(function(){
+        $('link').attr('href','style.css');
+    });
+});
+
+$(document).ready(function(){
+    $('.blueTheme').click(function(){
+        $('link').attr('href','style1.css');
+    });
+});
+
